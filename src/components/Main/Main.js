8ad59e6ea0a558d0eac9e39cb63ba932df353191
Dharmik <dharmikp.mobio@gmail.com>
2022-05-12 10:41:51 +0530

    navbar

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
+import Nav from './Nav.js'
+import NewDepositBtn from './NewDepositBtn.js'
+
+
+const Container = styled.div`
+    width: auto;
+    margin-left: 16rem;
+    position: relative;
+    padding: 0 4rem;
+`
+
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
+    return (
+        <Container>
+            <Nav />
+            <NewDepositBtn />
+        </Container>
+    )
 }
 
 export default Main
